a8eebd328ffc90e272154c57e8aa9eea240b0eee
Matheus Bessa <mathebessamo@gmail.com>
2025-09-09 16:53:43 -0300

    solution3

--- app/main.py
+++ app/main.py
@@ -2,8 +2,8 @@ from __future__ import annotations
 
 
 class Distance:
-    def __init__(self, kilometers: int | float) -> None:
-        self.km: float = float(kilometers)
+    def __init__(self, km: int | float) -> None:
+        self.km: float = float(km)
 
     def __str__(self) -> str:
         return f"Distance: {self.km} kilometers."
@@ -27,16 +27,16 @@ class Distance:
             return NotImplemented
         return self
 
-    def __mul__(self, multiplier: int | float) -> Distance:
-        if isinstance(multiplier, (int, float)):
-            return Distance(self.km * multiplier)
+    def __mul__(self, other: int | float) -> Distance:
+        if isinstance(other, (int, float)):
+            return Distance(self.km * other)
         return NotImplemented
 
-    def __truediv__(self, divisor: int | float) -> Distance:
-        if isinstance(divisor, (int, float)):
-            if divisor == 0:
+    def __truediv__(self, other: int | float) -> Distance:
+        if isinstance(other, (int, float)):
+            if other == 0:
                 raise ZeroDivisionError("division by zero")
-            return Distance(round(self.km / divisor, 2))
+            return Distance(round(self.km / other, 2))
         return NotImplemented
 
     def __lt__(self, other: Distance | int | float) -> bool:
